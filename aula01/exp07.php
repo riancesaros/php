d50ebb30c80fab227d1a317a9f8e092f194ad740
Rian Cesar Oliveira Souza <145462146+riancesaros@users.noreply.github.com>
2023-11-07 10:48:37 -0300

    Add files via upload

---
+++ aula01/exp07.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h2>PHP é divertido! </h2>";
+    echo "Olá mundo. <br>";
+    echo "Estou prestes a aprender PHP! <br>";
+    echo "Essa ","corda ","foi ","feita ","com multiplos parâmetros.";
+    echo "<br>";
+    print "Essa "."corda "."foi "."feita "."com multiplos parâmetros.";
+
+    print "<hr>";
+    ?>
+    <?php
+    $txt1 = "Aprenda PHP";
+    $txt2 = "IFBaiano.edu";
+    $x = 5;
+    $y = 4;
+
+    print "<h2>".$txt1."</h2>";
+    print "Estude PHP no ".$txt2."<br>";
+    print $x + $y;
+
+    ?>
+</body>
+</html>
