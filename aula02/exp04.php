af28b78f3bc1bb0eb8bc3f72166d6a6b2098f5d9
Rian Cesar Oliveira Souza <145462146+riancesaros@users.noreply.github.com>
2023-11-07 11:26:58 -0300

    Add files via upload

---
+++ aula02/exp04.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="exp04.css">
+    <title>Exemplo 4</title>
+</head>
+<body>
+    <div id="d1">
+        <h1>Titulo</h1>
+    </div>
+    <div id="d2">
+        <h1>Titulo</h1>
+    </div>
+    <div id="d3">
+        <h1>Titulo</h1>
+    </div>
+    <div id="d4">
+        <h1>Titulo</h1>
+    </div>
+
+</body>
+</html>
