8c8a493452cfdde7f9b127cbb58809bdc6414a13
Rian Cesar Oliveira Souza <riancesar.contact@gmail.com>
2024-04-01 09:46:09 -0300

    update

---
+++ 2conceitos_basicos/case_sensitive/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    // Não é case sensitive para as instruções da linguagem
+    echo "teste <br>";
+    eChO "testando 2 <br>";
+    ECHO "teste 3 <br>";
+
+    // É case sensitive para os nomes das variaveis 
+    $name = "rian";
+    $NAME = "rian2";
+
+    echo $name;
+    echo "<br>";
+    echo $NAME;
+?>
