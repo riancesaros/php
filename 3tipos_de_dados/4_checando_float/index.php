582c86030f389904f7c6edd48386354d39b1aabd
Rian Cesar Oliveira Souza <riancesar.contact@gmail.com>
2024-04-02 19:47:38 -0300

    update

---
+++ 3tipos_de_dados/4_checando_float/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    $a = "teste";
+    $b = 6.2;
+
+    if (is_float($a)) {
+        echo "é float sim man <br>";
+    }
+    else {
+        echo "esse numero não tem casa decimal brother. <br>";
+    }
+
+    if (is_float($b)) {
+        echo "é float sim man";
+    }
+    else {
+        echo "esse numero não tem casa decimal brother.";
+    }
+
+
+?>
