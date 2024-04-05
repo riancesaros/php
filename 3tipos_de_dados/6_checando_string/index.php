9b6670ec824f6161a51e5245e60f20a39ba1ccbc
Rian Cesar Oliveira Souza <riancesar.contact@gmail.com>
2024-04-04 22:34:58 -0300

    Create index.php

---
+++ 3tipos_de_dados/6_checando_string/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    $a = "teste";
+    $b = 9.5;
+
+    if (is_string($a)) {
+        echo "é sim sim brother <br>";
+    }
+    else {
+        echo "essa variavel não é uma string :( <br>";
+    }
+
+    if (is_string($b)) {
+        echo "é sim sim brother <br>";
+    }
+    else {
+        echo "essa variavel não é uma string :( <br>";
+    }
+
+
+?>
