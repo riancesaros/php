a6e0c8a0d76aa06851a040b49a992cd9a13211c1
Rian Cesar Oliveira Souza <riancesar.contact@gmail.com>
2024-04-05 17:26:21 -0300

    update

--- 3tipos_de_dados/6_checando_string/index.php
+++ 3tipos_de_dados/6_checando_string/index.php
@@ -4,14 +4,14 @@
     $b = 9.5;
 
     if (is_string($a)) {
-        echo "é sim sim brother <br>";
+        echo "é string sim brother <br>";
     }
     else {
         echo "essa variavel não é uma string :( <br>";
     }
 
     if (is_string($b)) {
-        echo "é sim sim brother <br>";
+        echo "é string sim brother <br>";
     }
     else {
         echo "essa variavel não é uma string :( <br>";
---
+++ 3tipos_de_dados/8_checando_booleanos/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    $a = true;
+    $b = false;
+
+    if (is_boolean($a)) {
+        echo "é verdadeiro <br>";
+    }
+    else {
+        echo "é falso =[ <br>";
+    }
+
+    if (is_boolean($b)) {
+        echo "é verdadeiro <br>";
+    }
+    else {
+        echo "é falso =[ <br>";
+    }
+
+
+?>
