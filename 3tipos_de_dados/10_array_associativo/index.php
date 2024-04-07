1e0790a920f8d86e1409cb91588b2c8ce099ea1d
Rian Cesar Oliveira Souza <riancesar.contact@gmail.com>
2024-04-06 23:27:06 -0300

    Create index.php

---
+++ 3tipos_de_dados/10_array_associativo/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    $a = ['nome' => 'rian', 'hobby' => 'xadrez', 'cor' => 'verde'];
+
+    echo "Olá, $a[nome]!";
+    echo "<br>";
+
+    echo "Você gosta de $a[hobby]?";
+    echo "<br>";
+
+?>
