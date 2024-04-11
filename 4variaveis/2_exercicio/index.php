09955d334eba97279b6921d23789bc25edcc79e1
Rian Cesar Oliveira Souza <riancesar.contact@gmail.com>
2024-04-10 22:25:06 -0300

    Create index.php

---
+++ 4variaveis/2_exercicio/index.php
@@ -0,0 +1,23 @@
+<?php
+
+    $meta = "corrida";
+    $distancia = 10;
+    $pausas = [2,4,6,8];
+
+    echo $meta;
+    echo "<br>";
+    echo $distancia ."km";
+    echo "<br>";
+
+    print_r($pausas);
+    echo "<br>";
+    var_dump($pausas);
+    echo "<br>";
+
+    //parte II - soma de numeros das variaveis
+    $feito = 5;
+    $afazer = 10;
+
+    $total = $feito + $afazer;
+    echo $total;
+?>
